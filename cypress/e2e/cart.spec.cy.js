ad70f6c865e4189820900dc8a19d4e28550536e5
Sumit Bhosale <sbhosale@frontlineed.com>
2026-03-26 16:45:13 +0530

    issue with cart file

--- cypress/e2e/cart.spec.cy.js
+++ cypress/e2e/cart.spec.cy.js
@@ -1,5 +1,6 @@
 /// <reference types="cypress" />
 import { login, addProductsTocart } from "../support/utils";
+import { selectors } from "../support/locators";
 describe('Add product to cart', () => {
  let testData;
  before(() => {
@@ -8,27 +9,22 @@ describe('Add product to cart', () => {
 });
 });
 beforeEach(() => {
-cy.visit('https://www.saucedemo.com/');
+cy.visit('/');
 login(testData.users.validUser.username, testData.users.validUser.password);
  });
 it ('should add products to cart and validte cart badge',() => {
  addProductsTocart(testData.products);
-        
-//Validate cart badge count
- cy.get('.shopping_cart_badge').should('contain', testData.products.length);
 
-<<<<<<< HEAD
-// Go to cart and validate products
-cy.get('.shopping-cart-link').click();
- testData.products.forEach(product =>{
- cy.contains('.cart_item', product).should('be.visible');
-=======
+//Validate cart badge count
+ cy.get(selectors.cart.cartBadge).should('contain', testData.products.length);
+
 // Go to cart 
 cy.get(selectors.cart.cartIcon).click();
+// validate products
  //validate products
  testData.products.forEach(product =>{
- cy.contains(selectors.cart.cartItem, product).should("be.visible");
->>>>>>> 0f8f936c24c2ab3a3e7a4745a34d53b13a1f4ed8
+ cy.contains(selectors.cart.cartItem, product).should('be.visible');
+
  })
  })
 });
--- cypress/support/utils.js
+++ cypress/support/utils.js
@@ -1,29 +1,16 @@
+import { selectors } from "./locators";
 export const login = (username,password) =>{    
-cy.get('#user-name').type(username);
-cy.get('#password').type(password);
-cy.get('#login-button').click();
+cy.get(selectors.login.username).type(username);
+cy.get(selectors.login.password).type(password);
+cy.get(selectors.login.loginBtn).click();
 };
 
 export const addProductsTocart = (products) =>{
-<<<<<<< HEAD
-    products.forEach(product => {
-    cy.get('.inventory_item_name')
-    .contains(product)
-    .find('button')
-    .should('be.visible')
-=======
     products.forEach((product) => {
     cy.contains(selectors.cart.cartItem, product)
-   .closest('.inventory_item')
+    closest('.inventory_item')
    .find(selectors.cart.addTocartBtn)
->>>>>>> 0f8f936c24c2ab3a3e7a4745a34d53b13a1f4ed8
     .click();
   
 });
 };
-// export const proceedToCheckout = () => {
-//     cy.get(selectors.cart.cartIcon).click();
-//     cy.get(selectors.cart.checkoutBtn).click();
-// };
-
-    
