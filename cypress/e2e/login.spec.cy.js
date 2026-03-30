80ca2640e9b9c40d68aa0c541279babdceae4623
Sumit Bhosale <sbhosale@frontlineed.com>
2026-03-30 14:51:53 +0530

    Updated login spec

--- cypress/e2e/login.spec.cy.js
+++ cypress/e2e/login.spec.cy.js
@@ -1,27 +1,30 @@
 /// <reference types="cypress" />
+import { selectors } from "../support/locators";
 import { login } from "../support/utils";
 
-describe('SauceDemo Test',()=> {
-let testData;
+describe('SauceDemo Test', () => {
+    let testData;
 
-before(() => {
-    cy.fixture('testData').then((data) =>{
-        testData = data;
-});
-});
-beforeEach(() => {
-cy.visit('https://www.saucedemo.com/');
-});
-  
-it('login successfully with valid user', ()=>{
-    login(testData.users.validUser.username, testData.users.validUser.password);
-    cy.url().should('include', '/inventory.html');
-});
-it('Inavlid user credential', () =>{
-    login(testData.users.invalidUser.username, testData.users.invalidUser.password);
-    cy.get('[data-test="error"]').should('be.visible')
-    .and('contain', 'Username and password do not match any user in this service');
-})
+    before(() => {
+        cy.fixture('testData').then((data) => {
+            testData = data;
+        });
+    });
+    beforeEach(() => {
+        cy.visit('/');
+    });
+//Positive Test
+    it('login successfully with valid user', () => {
+        login(testData.users.validUser.username, testData.users.validUser.password);
+        cy.url().should('include', '/inventory.html');
+        cy.get(selectors.inventory.inventoryList).should('be.visible');
+    });
+    //Invalid Credentials
+    it('Inavlid user credential', () => {
+        login(testData.users.invalidUser.username, testData.users.invalidUser.password);
+        cy.get(selectors.login.errorMsg).should('be.visible')
+            .and('contain', 'Username and password do not match any user in this service');
+    })
 });
 
 
